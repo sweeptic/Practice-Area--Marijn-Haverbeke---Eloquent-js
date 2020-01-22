1d50c88ef7728711f4fa350477b321531c44f0a0
Attila Horvath <sweeptic@gmail.com>
2020-01-22 14:02:50 +0100

    add files

---
+++ dom.js
@@ -0,0 +1,13 @@
+function talksAbout(node, string) {
+    if (node.nodeType == Node.ELEMENT_NODE) {
+        for (let i = 0; i < node.childNodes.length; i++) {
+            if (talksAbout(node.childNodes[i], string)) {
+                return true;
+            }
+        }
+        return false;
+    } else if (node.nodeType == Node.TEXT_NODE) {
+        return node.nodeValue.indexOf(string) > -1;
+    }
+}
+console.log(talksAbout(document.body, "book"));
