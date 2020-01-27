b8a9691c6d882ac960362b2cff3410ee22b9e196
Attila Horvath <sweeptic@gmail.com>
2020-01-27 10:11:14 +0100

    remove files

--- eloq_proba.js
+++ eloq_proba.js
@@ -1,1 +1,12 @@
-console.log("hah");
+
+
+let r = "abs"
+let a = []
+
+let _str = Object.prototype.toString;
+//console.log(r.toString());
+
+ //console.log( _str.call(r));
+
+//console.log( toString.call(a));
+console.log( r.toString);
